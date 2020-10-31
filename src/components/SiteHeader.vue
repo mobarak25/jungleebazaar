c6af94b4415e67186817cb0397b2821648274a1d
unknown <mobarakali62@gmail.com>
2020-10-31 17:03:22 +0600

    header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,7 +2,7 @@
     <header>
         <div class="top-header-wrapper">
             <div class="container">
-                <div class="top-header d-flex flex-wrap">
+                <div class="top-header d-flex flex-wrap align-items-center">
                     <div class="top-header-left">
                         <router-link :to="{ name: 'Home' }"
                             ><img :src="`${$root.publicPath}images/logo.png`"
@@ -73,12 +73,79 @@
                 </div>
             </div>
         </div>
+        <div class="btm-header-wrapper">
+            <div class="container">
+                <div
+                    class="btm-header d-flex flex-wrap justify-content-between"
+                >
+                    <nav
+                        class="main-menu d-flex flex-wrap justify-content-center align-items-center"
+                    >
+                        <ul>
+                            <li
+                                v-for="nav in navs"
+                                :key="nav.id"
+                                :class="{
+                                    'has-child js-has-child': nav.hasChild,
+                                }"
+                            >
+                                <a href="#">
+                                    {{ nav.name }}
+                                    <i
+                                        v-if="nav.hasChild"
+                                        class="icofont-rounded-down"
+                                    ></i>
+                                </a>
+                                <ul v-if="nav.hasChild">
+                                    <li
+                                        v-for="child in nav.childs"
+                                        :key="child.id"
+                                    >
+                                        <a href>{{ child.name }}</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <a class="more-menu" href="#">More..</a>
+                    </nav>
+
+                    <a class="post-add" href="#"
+                        ><i class="icofont-plus"></i> Post Your Ad</a
+                    >
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
     name: "SiteHeader",
+    data() {
+        return {
+            navs: [
+                { id: 1, name: "Drop shipping", hasChild: false },
+                { id: 2, name: "Bulk selling", hasChild: false },
+                { id: 3, name: "Warehouse Products", hasChild: false },
+                {
+                    id: 4,
+                    name: "Warehouse Closeouts",
+                    hasChild: true,
+                    childs: [
+                        { id: 1, name: "X Monica" },
+                        { id: 2, name: "X Monalisa" },
+                        { id: 3, name: "X Alexander" },
+                        { id: 4, name: "X Monica" },
+                        { id: 5, name: "X Monalisa" },
+                        { id: 6, name: "X Alexander" },
+                    ],
+                },
+                { id: 5, name: "Overstock Surplus", hasChild: false },
+                { id: 6, name: "Wholesale Stock", hasChild: false },
+                { id: 7, name: "Manufacturer Stock", hasChild: false },
+            ],
+        };
+    },
     methods: {},
 };
 </script>
